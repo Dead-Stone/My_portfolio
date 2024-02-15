f09d8e297a083b2331daaa6a1dd641ad3ecf79e1
M Mohana Satyanarayana <mohana4984@gmail.com>
2024-02-15 03:43:30 -0800

    Updated the text info

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+var myCarousel = document.querySelector('#carouselExperience')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 5000,
+  wrap: true
+})
+var myCarousel = document.querySelector('#carouselExperience');
+
+document.querySelector('.carousel-control-prev').addEventListener('click', function () {
+  var carousel = bootstrap.Carousel.getInstance(myCarousel);
+  carousel.prev();
+});
+
+document.querySelector('.carousel-control-next').addEventListener('click', function () {
+  var carousel = bootstrap.Carousel.getInstance(myCarousel);
+  carousel.next();
+});
